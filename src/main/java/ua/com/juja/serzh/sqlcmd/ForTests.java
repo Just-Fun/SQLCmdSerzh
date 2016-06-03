93869c0ed1bb1205f07eb959af42dc76f3a13f45
serzh <Just-Fun@users.noreply.github.com>
2016-06-03 22:27:45 +0300

    Вернулся :)

--- src/main/java/ua/com/juja/serzh/sqlcmd/ForTests.java
+++ src/main/java/ua/com/juja/serzh/sqlcmd/ForTests.java
@@ -17,6 +17,7 @@ public class ForTests {
         View view = new Console();
         DatabaseManager manager = new PostgresManager();
         manager.connect("sqlcmd5database","postgres", "postgres");
+
         view.write(manager.getTableNames().toString());
         view.write(manager.getDatabases().toString());
 

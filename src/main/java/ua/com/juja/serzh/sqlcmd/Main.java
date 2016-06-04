7ada110944bd03eaadd088002ffbbc04e9374bf4
serzh <Just-Fun@users.noreply.github.com>
2016-06-04 18:09:34 +0300

    проба

--- src/main/java/ua/com/juja/serzh/sqlcmd/Main.java
+++ src/main/java/ua/com/juja/serzh/sqlcmd/Main.java
@@ -20,6 +20,7 @@ public class Main {
         DatabaseManager manager = new PostgresManager();
         MainController controller = new MainController(view, manager);
         controller.run();
+
     }
     // connect|postgres|postgres|postgres
 }

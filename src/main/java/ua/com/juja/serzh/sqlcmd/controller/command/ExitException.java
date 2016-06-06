9b1539adbcaf7ecf7aa26117e66e96e0609ec220
serzh <Just-Fun@users.noreply.github.com>
2016-06-06 09:41:21 +0300

    TODO

--- src/main/java/ua/com/juja/serzh/sqlcmd/controller/command/ExitException.java
+++ src/main/java/ua/com/juja/serzh/sqlcmd/controller/command/ExitException.java
@@ -3,5 +3,7 @@ package ua.com.juja.serzh.sqlcmd.controller.command;
 /**
  * Created by serzh on 5/11/16.
  */
+// TODO: 06.06.16  Если ты хочешь протестировать свой собственный exception, то напиши тест на метод,
+//который его кидает с параметром expected=myexception
 public class ExitException extends RuntimeException {
 }

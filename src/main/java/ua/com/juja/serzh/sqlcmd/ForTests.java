3984088ec8e6e530e26a1e3ec7d1c96dbaec0982
Just-Fun <Just-Fun@users.noreply.github.com>
2016-06-09 15:40:47 +0300

    Балуюсь...

--- src/main/java/ua/com/juja/serzh/sqlcmd/ForTests.java
+++ src/main/java/ua/com/juja/serzh/sqlcmd/ForTests.java
@@ -1,25 +1,13 @@
 package ua.com.juja.serzh.sqlcmd;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.math.BigDecimal;
+import java.util.*;
 
 /**
  * Created by serzh on 6/9/16.
  */
 public class ForTests {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<>(Arrays.asList("Luck", "yoda"));
-        List<String> list1 = new ArrayList<>(Arrays.asList("Luck", "yoda"));
 
-        for (String l : list) {
-            if (!Character.isLowerCase(l.charAt(0))) {
-                continue;
-            }
-            list1.add(l);
-        }
-
-        System.out.println(list.toString());
-        System.out.println(list1.toString());
     }
 }

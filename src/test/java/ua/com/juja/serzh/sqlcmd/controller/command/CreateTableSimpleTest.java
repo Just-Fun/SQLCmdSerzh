b02bde812ca72a9bc25cca72e805ca6a9dd15b00
serzh <Just-Fun@users.noreply.github.com>
2016-07-06 12:27:12 +0300

    добавил  в тест CreateTableSimpleTest 2 метода, использовал мультиплай вызов when(view.read()).thenReturn

--- src/test/java/ua/com/juja/serzh/sqlcmd/controller/command/CreateTableSimpleTest.java
+++ src/test/java/ua/com/juja/serzh/sqlcmd/controller/command/CreateTableSimpleTest.java
@@ -3,6 +3,8 @@ package ua.com.juja.serzh.sqlcmd.controller.command;
 import org.junit.Before;
 import org.junit.Test;
 import ua.com.juja.serzh.sqlcmd.model.DatabaseManager;
+import ua.com.juja.serzh.sqlcmd.model.PostgresManager;
+import ua.com.juja.serzh.sqlcmd.view.Console;
 import ua.com.juja.serzh.sqlcmd.view.View;
 
 import static org.junit.Assert.*;
@@ -16,11 +18,13 @@ import static org.mockito.Mockito.when;
 public class CreateTableSimpleTest {
 
     private Command command;
+    private View view;
+    private DatabaseManager manager;
 
     @Before
     public void setup() {
-        DatabaseManager manager = mock(DatabaseManager.class);
-        View view = mock(View.class);
+        manager = mock(PostgresManager.class);
+        view = mock(Console.class);
         command = new CreateTableSimple(manager, view);
     }
 
@@ -34,5 +38,28 @@ public class CreateTableSimpleTest {
     public void testProcessWithWrongCommand() throws Exception {
         boolean canProcess = command.canProcess("createTable34");
         assertFalse(canProcess);
+    }
+
+    @Test
+    public void testExit() throws Exception {
+        when(view.read()).thenReturn("0");
+        command.process("createTable");
+
+        verify(view).write(("Выход в основное меню"));
+    }
+
+    @Test
+    public void testCreateOnlyIdColumn() throws Exception {
+        when(view.read()).thenReturn("users5")
+                .thenReturn("id")
+                .thenReturn("5");
+        command.process("createTable");
+
+        verify(view).write(("Введите имя для создаваемой таблицы(имя должно начинаться с буквы) или '0' для выхода в основное меню"));
+        verify(view).write(("Имя новой базы: users5"));
+        verify(view).write(("Введите имя для колонки PRIMARY KEY(имя должно начинаться с буквы) или '0' для выхода в основное меню"));
+        verify(view).write(("Имя колонки PRIMARY KEY: id"));
+        verify(view).write(("Введите имя для еще одной колонки(имя должно начинаться с буквы) или '5' для создания базы с введенными колонками или '0' для выхода в основное меню"));
+        verify(view).write(("Таблица users5 была успешно создана."));
     }
 }

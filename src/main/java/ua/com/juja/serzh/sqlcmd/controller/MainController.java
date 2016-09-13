e01e647da37c58f51391760f5f44f88d2bff3174
Just-Fun <Just-Fun@users.noreply.github.com>
2016-09-13 16:35:16 +0300

    foreach changed to lambda

--- src/main/java/ua/com/juja/serzh/sqlcmd/controller/MainController.java
+++ src/main/java/ua/com/juja/serzh/sqlcmd/controller/MainController.java
@@ -8,7 +8,6 @@ import ua.com.juja.serzh.sqlcmd.view.View;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by indigo on 25.08.2015.
